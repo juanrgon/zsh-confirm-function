3fd0b1f1e9b1cf96f76172b9ed5644391b20be1a
Juan Gonzalez <jrg2156@gmail.com>
2018-07-02 04:01:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+zsh-confirm-function
+
+Plugin that adds a `confirm` function wrapper that can be applied to commands to force users to confirm execution of the command before running it.
+
+```sh
+>
+> confirm git push --force
+> git push --force
+Are you sure you want to do this? [y/N]:
+```
---
+++ confirm.plugin.zsh
@@ -0,0 +1,41 @@
+export CONFIRM_COMMAND_LIST=()
+
+confirm () {
+    _confirm () {
+        local respone
+        print -P "%B%F{yellow}Are you sure want to do this%f%b? [y/N] \c"
+        read -r response
+        case "$response" in
+            [yY][eE][sS]|[yY])
+                true
+                ;;
+            *)
+                false
+                ;;
+        esac
+    }
+
+    _custom_accept_line () {
+        local command
+        for command in $CONFIRM_COMMAND_LIST
+            do
+                if [[ "$BUFFER" = *"$command" ]]; then
+                    BUFFER="_confirm && $command"
+                fi
+            done
+        zle .accept-line
+    }
+    zle -N accept-line _custom_accept_line
+
+    local command=$*
+    found=false
+    for command_in_list in $CONFIRM_COMMAND_LIST
+        do
+            if [[ $command_in_list == $command ]]; then
+                found=true
+            fi
+        done
+    if [[ $found ]]; then
+        export CONFIRM_COMMAND_LIST+=command
+    fi
+}
