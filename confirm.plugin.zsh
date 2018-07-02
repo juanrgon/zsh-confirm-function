0bbbc15f96a331b54ca90a837d095936559697a6
Juan Gonzalez <jrg2156@gmail.com>
2018-07-02 05:27:51 -0400

    Update

--- confirm.plugin.zsh
+++ confirm.plugin.zsh
@@ -2,7 +2,7 @@ export CONFIRM_COMMAND_LIST=()
 
 confirm () {
     _confirm () {
-        local respone
+        local response
         print -P "%B%F{yellow}Are you sure want to do this%f%b? [y/N] \c"
         read -r response
         case "$response" in
@@ -15,27 +15,31 @@ confirm () {
         esac
     }
 
+    local command=$*
+    not_found=true
+    for command_in_list in $CONFIRM_COMMAND_LIST
+        do
+            if [[ $command_in_list == $command ]]; then
+                not_found=false
+            fi
+        done
+    if [[ $not_found ]]; then
+        CONFIRM_COMMAND_LIST+=$command
+    fi
+
+
     _custom_accept_line () {
         local command
+        function join_by { local IFS="$1"; shift; echo "$*"; }
+        spaceless_buffer=$(join_by ' ' ${=BUFFER})
         for command in $CONFIRM_COMMAND_LIST
             do
-                if [[ "$BUFFER" = *"$command" ]]; then
+                if [[ "$spaceless_buffer" = *"$command" ]]; then
                     BUFFER="_confirm && $command"
+                    break
                 fi
             done
         zle .accept-line
     }
     zle -N accept-line _custom_accept_line
-
-    local command=$*
-    found=false
-    for command_in_list in $CONFIRM_COMMAND_LIST
-        do
-            if [[ $command_in_list == $command ]]; then
-                found=true
-            fi
-        done
-    if [[ $found ]]; then
-        CONFIRM_COMMAND_LIST+=command
-    fi
 }
