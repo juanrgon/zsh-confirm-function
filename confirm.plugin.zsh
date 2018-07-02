208170f61c93fd530bbe5fc73142683992ceb7c7
Juan Gonzalez <jrg2156@gmail.com>
2018-07-02 09:34:30 -0400

    Fix

--- confirm.plugin.zsh
+++ confirm.plugin.zsh
@@ -1,20 +1,6 @@
 export CONFIRM_COMMAND_LIST=()
 
 confirm () {
-    _confirm () {
-        local response
-        print -P "%B%F{yellow}Are you sure want to do this%f%b? [y/N] \c"
-        read -r response
-        case "$response" in
-            [yY][eE][sS]|[yY])
-                true
-                ;;
-            *)
-                false
-                ;;
-        esac
-    }
-
     local command=$*
     not_found=true
     for command_in_list in $CONFIRM_COMMAND_LIST
@@ -26,20 +12,25 @@ confirm () {
     if [[ $not_found ]]; then
         CONFIRM_COMMAND_LIST+=$command
     fi
-
-
-    _custom_accept_line () {
-        local command
-        function join_by { local IFS="$1"; shift; echo "$*"; }
-        spaceless_buffer=$(join_by ' ' ${=BUFFER})
-        for command in $CONFIRM_COMMAND_LIST
-            do
-                if [[ "$spaceless_buffer" = "$command"* ]]; then
-                    BUFFER="_confirm && $spaceless_buffer"
-                    break
-                fi
-            done
-        zle .accept-line
-    }
-    zle -N accept-line _custom_accept_line
 }
+
+_custom_accept_line () {
+    local command
+    function join_by { local IFS="$1"; shift; echo "$*"; }
+    spaceless_buffer=$(join_by ' ' ${=BUFFER})
+    for command in $CONFIRM_COMMAND_LIST
+        do
+            if [[ "$spaceless_buffer" =~ "$command"  ]]; then
+                print -P "\n%B%F{yellow}Are you sure want to do this?%f%b [y/N] \c"
+                if read -q; then
+                    BUFFER=$spaceless_buffer
+                else
+                    zle -I
+                    BUFFER=''
+                fi
+                break
+            fi
+        done
+    zle .accept-line
+}
+zle -N accept-line _custom_accept_line
