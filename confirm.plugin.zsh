6b055bbac412f9ea71c3373bb2775cc35fd40168
Juan Gonzalez <jrg2156@gmail.com>
2018-07-16 02:07:01 -0400

    Fix

--- confirm.plugin.zsh
+++ confirm.plugin.zsh
@@ -1,36 +1,31 @@
-export CONFIRM_COMMAND_LIST=()
+typeset -gAH CONFIRM_COMMANDS
 
 confirm () {
-    local command=$*
-    not_found=true
-    for command_in_list in $CONFIRM_COMMAND_LIST
-        do
-            if [[ $command_in_list == $command ]]; then
-                not_found=false
-            fi
-        done
-    if [[ $not_found ]]; then
-        CONFIRM_COMMAND_LIST+=$command
-    fi
+    local _command
+    _command=$(echo $1 | awk '{$1=$1};1')
+    CONFIRM_COMMANDS[$_command]=$2
 }
 
-_custom_accept_line () {
-    local command
-    function join_by { local IFS="$1"; shift; echo "$*"; }
-    spaceless_buffer=$(join_by ' ' ${=BUFFER})
-    for command in $CONFIRM_COMMAND_LIST
-        do
-            if [[ "$spaceless_buffer" =~ "$command"  ]]; then
-                print -P "\n%B%F{yellow}Are you sure want to do this?%f%b [y/N] \c"
-                if read -q; then
-                    BUFFER=$spaceless_buffer
-                else
-                    zle -I
-                    BUFFER=''
-                fi
-                break
+custom-accept-line () {
+    local _command
+    local _trimmed_buffer
+    _trimmed_buffer=$(echo $BUFFER | awk '{$1=$1};1')
+    for _command message in ${(kv)CONFIRM_COMMANDS}; do
+        _trimmed_command=$(echo $_command | awk '{$1=$1};1')
+        if [[ "$_trimmed_buffer" =~ "$_trimmed_command" ]]; then
+            extra_message=$CONFIRM_COMMANDS[$_trimmed_buffer]
+            if [[ -n $extra_message ]]; then
+                print -P "\n%B%F{yellow}$extra_message%f%b"
             fi
-        done
+            print -P "%B%F{yellow}Are you sure want to do this?%f%b [y/N]"
+            if read -q; then
+                BUFFER=$_trimmed_buffer
+            else
+                BUFFER=''
+            fi
+            break
+        fi
+    done
     zle .accept-line
 }
-zle -N accept-line _custom_accept_line
+zle -N accept-line custom-accept-line
